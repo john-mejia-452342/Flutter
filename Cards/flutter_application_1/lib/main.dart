d84dd0581230e2b85ea66291aec824e7e4c659e3
unknown <SENA@SGLDFPCATSD254.FormacionSena.RED>
2023-11-21 17:11:34 -0500

    Second Commit

--- Cards/flutter_application_1/lib/main.dart
+++ Cards/flutter_application_1/lib/main.dart
@@ -1,58 +1,280 @@
+
+
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [Card].
+void main() {
+  runApp(const MyApp());
+}
 
-void main() => runApp(const CardExampleApp());
-
-class CardExampleApp extends StatelessWidget {
-  const CardExampleApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color.fromARGB(71, 85, 255, 7),
-        appBar: AppBar(title: const Text('Card Sample'),),
-        body: const CardExample(),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: const Text(
+            'Simple Card desing',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          centerTitle: true,
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TarjetaGaleria(),
+            TarjetaReloj(),
+            TarjetaAccesibilidad(),
+          ],
+        ),
       ),
     );
   }
 }
 
-class CardExample extends StatelessWidget {
-  const CardExample({super.key});
+class TarjetaGaleria extends StatelessWidget {
+  const TarjetaGaleria({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
+    return Container(
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 255, 171, 44),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
       child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Titulo'),
-              subtitle: Text(
-                  'Este es el subtitulo del card. Aqui podemos colocar descripcion de este card.'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('Aceptar', style: TextStyle(color: Colors.black87),),
-                  onPressed: () {/* ... */},
+        color: Colors.transparent,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              const Icon(Icons.image, color: Colors.black38, size: 80),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Photo Album',
+                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      'TWICE',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Edit',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Create',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Delete',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('Cancelar', style: TextStyle(color: Colors.black87),),
-                  onPressed: () {/* ... */},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TarjetaReloj extends StatelessWidget {
+  const TarjetaReloj({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 66, 189, 70),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Card(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              const Icon(Icons.schedule, color: Colors.black38, size: 80),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Time',
+                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      'TWICE',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Edit',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Create',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Delete',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 10),
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TarjetaAccesibilidad extends StatelessWidget {
+  const TarjetaAccesibilidad({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 212, 70, 248),
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2),
+            spreadRadius: 3,
+            blurRadius: 3,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Card(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: [
+              const Icon(Icons.accessibility, color: Colors.black38, size: 80),
+              const SizedBox(width: 15),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Access',
+                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 15),
+                    const Text(
+                      'TWICE ',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Edit',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Create',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                          },
+                          child: const Text(
+                            'Delete',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- Dialog/flutter_application_1/lib/main.dart
+++ Dialog/flutter_application_1/lib/main.dart
@@ -1,278 +1,58 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+/// Flutter code sample for [Card].
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+void main() => runApp(const CardExampleApp());
+
+class CardExampleApp extends StatelessWidget {
+  const CardExampleApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: const Text(
-            'Simple Card desing',
-            style: TextStyle(color: Colors.white, fontSize: 20),
-          ),
-          centerTitle: true,
-        ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        backgroundColor: Color.fromARGB(71, 85, 255, 7),
+        appBar: AppBar(title: const Text('Card Sample'),),
+        body: const CardExample(),
+      ),
+    );
+  }
+}
+
+class CardExample extends StatelessWidget {
+  const CardExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Card(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            TarjetaGaleria(),
-            TarjetaReloj(),
-            TarjetaAccesibilidad(),
+            const ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Titulo'),
+              subtitle: Text(
+                  'Este es el subtitulo del card. Aqui podemos colocar descripcion de este card.'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                TextButton(
+                  child: const Text('Aceptar', style: TextStyle(color: Colors.black87),),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 8),
+                TextButton(
+                  child: const Text('Cancelar', style: TextStyle(color: Colors.black87),),
+                  onPressed: () {/* ... */},
+                ),
+                const SizedBox(width: 10),
+              ],
+            ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class TarjetaGaleria extends StatelessWidget {
-  const TarjetaGaleria({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 255, 171, 44),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 3,
-            blurRadius: 3,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Card(
-        color: Colors.transparent,
-        elevation: 0,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              const Icon(Icons.image, color: Colors.white, size: 80),
-              const SizedBox(width: 15),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Photo Album',
-                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      'TWICE',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Edit',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Create',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Delete',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TarjetaReloj extends StatelessWidget {
-  const TarjetaReloj({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 66, 189, 70),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 3,
-            blurRadius: 3,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Card(
-        color: Colors.transparent,
-        elevation: 0,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              const Icon(Icons.schedule, color: Colors.white, size: 80),
-              const SizedBox(width: 15),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Time',
-                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      'TWICE',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Edit',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Create',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Delete',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class TarjetaAccesibilidad extends StatelessWidget {
-  const TarjetaAccesibilidad({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 212, 70, 248),
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.2),
-            spreadRadius: 3,
-            blurRadius: 3,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Card(
-        color: Colors.transparent,
-        elevation: 0,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Row(
-            children: [
-              const Icon(Icons.accessibility, color: Colors.white, size: 80),
-              const SizedBox(width: 15),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      'Access',
-                      style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(height: 15),
-                    const Text(
-                      'TWICE ',
-                      style: TextStyle(color: Colors.white, fontSize: 15),
-                    ),
-                    const SizedBox(height: 15),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Edit',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Create',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                          },
-                          child: const Text(
-                            'Delete',
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
         ),
       ),
     );
